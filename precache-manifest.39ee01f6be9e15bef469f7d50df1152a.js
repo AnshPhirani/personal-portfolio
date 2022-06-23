39506ade63240e438a1474658c7eef8d2388a4fa
Ansh Phirani <anshphirani@gmail.com>
2022-06-24 00:05:26 +0530

    Updates

---
+++ precache-manifest.39ee01f6be9e15bef469f7d50df1152a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09019d522f16845ace27b917c6d61c5",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "3d7a71fe96fe19be09e4",
+    "url": "/personal-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5d1d304abe6094caea8b",
+    "url": "/personal-portfolio/static/css/main.49652da8.chunk.css"
+  },
+  {
+    "revision": "3d7a71fe96fe19be09e4",
+    "url": "/personal-portfolio/static/js/2.559f7165.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/personal-portfolio/static/js/2.559f7165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1d304abe6094caea8b",
+    "url": "/personal-portfolio/static/js/main.489d5fee.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  }
+]);
