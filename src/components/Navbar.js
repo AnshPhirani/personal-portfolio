c6ed9b034d3f77ea34e67354452bd8fb74488246
Ansh Phirani <anshphirani@gmail.com>
2022-06-23 23:37:14 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function Navbar({aboutRef, projectsRef, skillsRef}) {
+
+    const scrollToRef = ref => {
+      window.scrollTo({ top: ref.current.offsetTop, behavior : "smooth"})
+    }
+
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        if(e.target.name === "skills"){
+          scrollToRef(skillsRef);
+        }
+        else if(e.target.name === "projects"){
+          scrollToRef(projectsRef);
+        }
+        else if(e.target.name === "about"){
+          scrollToRef(aboutRef);
+        }
+    }
+
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light  nav-custom">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active mx-5 mt-4">
+            <a className="nav-link" name="about" onClick={handleOnClick} href="/">
+              About Me
+            </a>
+          </li>
+          <li className="nav-item active mx-5 mt-4">
+            <a className="nav-link" name="projects" onClick={handleOnClick} href="/">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item active mx-5 mt-4">
+            <a className="nav-link" name ="skills" onClick={handleOnClick} href="/">
+              Skills
+            </a>
+          </li>
+          <li className="nav-item active mx-5 mt-4">
+            <a className="nav-link" onClick={handleOnClick} href="/">
+              Download Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
